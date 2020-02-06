7ed2da830c3bc227858caf405aaf73b70918aa4a
iprod5 <franklinluther3@gmail.com>
2020-02-06 17:16:16 +0000

    added existing compute env

--- jobQueue.tf
+++ jobQueue.tf
@@ -1,5 +1,16 @@
+provider "aws" {
+region = "us-east-2"
+}
+
+
+data "aws_batch_compute_environment" "compute_environment" {
+ 
+}
+
+
+
 resource "aws_batch_job_queue" "job_queue" {
-  name                 = "tf-test-batch-job-queue"
+  name                 = "tf-batch-job-queue"
   state                = "ENABLED"
   priority             = 10
   compute_environments = ["${aws_batch_compute_environment.compute_environments.arn}"]
